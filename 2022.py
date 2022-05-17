8c2bf1f2d35bb17f40b8bb6e67d00b676937efb1
25mbraun <105305278+25mbraun@users.noreply.github.com>
2022-05-17 09:42:26 -0700

    Update 2022.py

--- 2022.py
+++ 2022.py
@@ -1,9 +1,34 @@
 from djitellopy import tello
-
 from time import sleep
 
-me = tello.Tello
+M1 = tello.Tello()
+M1.connect()
+print(M1.get_battery())
 
-me.connect()
+M1.takeoff()
 
-print(me.get_battery())
+#  Starting Takeoff Point
+M1.move_up(95)
+M1.move_forward(400)
+sleep(.5)
+M1.move_forward(400)
+sleep(1)
+
+# Moving in to the flying zone
+M1.rotate_counter_clockwise(90)
+M1.move_forward(350)
+sleep(1)
+
+# Going to start
+M1.Rotate_counter_clockwise(90)
+M1.move_forward(350)
+sleep(.5)
+M1.move_forward(350)
+sleep(1)
+
+# Landing Zone for the drone
+M1.rotate_counter_clockwise(90)
+M1.move_forward(370)
+sleep(1)
+
+M1.Land()
